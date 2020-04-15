4a5ba96dd2026d05f4ff09e5391026f3e9ba9be5
markfrias <friasmarkjabel@gmail.com>
2020-04-15 16:15:27 +0800

    Add initial HTML and CSS

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrendingComponent } from './trending/trending.component';
+import { PostsComponent } from './posts/posts.component';
+import { TagsComponent } from './tags/tags.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrendingComponent,
+    PostsComponent,
+    TagsComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
