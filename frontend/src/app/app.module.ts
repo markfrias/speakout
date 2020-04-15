76e00f5a59f4bb772e0bbd8629f54b7c4fb9b559
markfrias <friasmarkjabel@gmail.com>
2020-04-16 00:09:28 +0800

    Add initial routes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrendingComponent } from './trending/trending.component';
 import { PostsComponent } from './posts/posts.component';
 import { TagsComponent } from './tags/tags.component';
 import { TeamComponent } from './team/team.component';
+
+const appRoutes: Routes = [
+  { path: 'trending', component: TrendingComponent },
+  { path: 'posts', component: PostsComponent }, 
+  { path: 'tags', component: TagsComponent} ,
+  { path: 'team', component: TeamComponent}
+]
 
 @NgModule({
   declarations: [
@@ -18,7 +25,11 @@ import { TeamComponent } from './team/team.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
