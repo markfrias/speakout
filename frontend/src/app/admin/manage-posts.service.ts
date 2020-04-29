7e7e037a659afa536b4d5b37f010d18ccfcd1d2c
Tatenda07 <54758370+Tatenda07@users.noreply.github.com>
2020-04-29 14:16:23 +0800

    add author in the blog collection

---
+++ frontend/src/app/admin/manage-posts.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ManagePostsService {
+
+  constructor(private http: HttpClient) { }
+
+  getPosts() {
+    return this.http.get('http://127.0.0.1:3300/posts/');
+  }
+
+}
