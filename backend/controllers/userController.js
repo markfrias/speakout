41ae0820cb659d5bf572c05df0d0eb82a77826ce
Tatenda07 <54758370+Tatenda07@users.noreply.github.com>
2020-04-30 16:06:32 +0800

    encrypt user passwords before saving them into the database using bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const ObjectId = require('mongoose').Types.ObjectId;
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
 
 var { User } = require('../models/user.model');
 
@@ -34,47 +36,60 @@ router.get('/:id', (req, res) => {
 
 //add user into the database
 router.post('/',(req, res) => {
-    var newUser = new User({
-        fname: req.body.fname,
-        lname: req.body.lname,
-        username: req.body.username,
-        email: req.body.email,
-        address: req.body.address,
-        phoneNumber: req.body.phoneNumber,
-        password: req.body.password,
-        role: req.body.role,
-    });
-    newUser.save((err, doc) => {
-        if (!err) 
-            res.send(doc);
-        else
-            console.log('Error in adding new user: ' + JSON.stringify(err, undefined, 2));
-    })
-});
-
-//edit or update user information
-router.put('/:id', (req, res) => {
-    //check if the user exists in the database
-    User.exists({ _id: req.params.id }).then((result) => {
-        if (!result) {
-            return res.status(400).send(`No user record with given id: ${req.params.id}`);
-        } else {
-            var updateUser = {
+    bcrypt.hash(req.body.password, saltRounds, (error, hash) => {
+        if (!error) {
+            var newUser = new User({
                 fname: req.body.fname,
                 lname: req.body.lname,
                 username: req.body.username,
                 email: req.body.email,
                 address: req.body.address,
                 phoneNumber: req.body.phoneNumber,
-                password: req.body.password,
+                password: hash,
                 role: req.body.role,
-            };
-            User.findByIdAndUpdate(req.params.id, { $set: updateUser }, { new: true }, (err, doc) => {
-                if (!err)
+            });
+        
+            newUser.save((err, doc) => {
+                if (!err) 
                     res.send(doc);
                 else
-                    console.log('Error in User update: ' + JSON.stringify(err, undefined, 2));
-            });
+                    console.log('Error in adding new user: ' + JSON.stringify(err, undefined, 2));
+            })
+        } else {
+            console.log('Error in password encryption: ' + JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+//edit or update user information
+router.put('/:id', (req, res) => {
+    bcrypt.hash(req.body.password, saltRounds, (error, hash) => {
+        if (!error) {
+            //check if the user exists in the database
+            User.exists({ _id: req.params.id }).then((result) => {
+                if (!result) {
+                    return res.status(400).send(`No user record with given id: ${req.params.id}`);
+                } else {
+                    var updateUser = {
+                        fname: req.body.fname,
+                        lname: req.body.lname,
+                        username: req.body.username,
+                        email: req.body.email,
+                        address: req.body.address,
+                        phoneNumber: req.body.phoneNumber,
+                        password: hash,
+                        role: req.body.role,
+                    };
+                    User.findByIdAndUpdate(req.params.id, { $set: updateUser }, { new: true }, (err, doc) => {
+                        if (!err)
+                            res.send(doc);
+                        else
+                            console.log('Error in User update: ' + JSON.stringify(err, undefined, 2));
+                    });
+                }
+            }); 
+        } else {
+            console.log('Error in password encryption, could not update user information: ' + JSON.stringify(err, undefined, 2));
         }
     });
 });
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 var User = mongoose.model('User', {
-    fname: { type: String },
-    lname: { type: String},
-    username: { type: String },
-    email: { type: String },
+    fname: { type: String, required: true },
+    lname: { type: String, required: true},
+    username: { type: String, required: true },
+    email: { type: String, required: true },
     address: { type: String },
     phoneNumber: {type: Number},
-    password: { type: String },
+    password: { type: String, required: true },
     role: { type: String },
 });
+
+//generating a hash
+User.generateHash = (password) => {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(12));
+}
+
+//cheking if password is valid on login
+/* we are not going to use this code for now
+User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.localPassword);
+}
+*/
 
 module.exports = { User };
