8130dadcdf6c66e237d0b549da24928acd0843a6
Tatenda07 <54758370+Tatenda07@users.noreply.github.com>
2020-05-12 16:35:12 +0800

    login fix

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,24 +6,13 @@ import { UserService } from './shared/user.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [UserService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'frontend';
   userDetails;
 
-  constructor(public userService: UserService, private router: Router){
-
-  }
-
-  ngOnInit(){
-    this.userService.getUserProfile().subscribe(
-      res => {
-        this.userDetails = res['user'];
-      }
-    );
-  }
+  constructor(public userService: UserService, private router: Router){ }
 
   onLogout(){
     this.userService.deleteToken();
