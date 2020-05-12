3a19d1aa1974338abb2e9cc0c012a69ff45fa623
Tatenda07 <54758370+Tatenda07@users.noreply.github.com>
2020-05-12 15:52:08 +0800

    Show username (with dashboard link and logout link) instead of login button when the user is logged in

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService } from './shared/user.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
 export class AppComponent {
   title = 'frontend';
+  userDetails;
+
+  constructor(public userService: UserService, private router: Router){
+
+  }
+
+  ngOnInit(){
+    this.userService.getUserProfile().subscribe(
+      res => {
+        this.userDetails = res['user'];
+      }
+    );
+  }
+
+  onLogout(){
+    this.userService.deleteToken();
+    this.router.navigate(['/posts']);
+  }
+
 }
