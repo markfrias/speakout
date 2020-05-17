694ea8a32fe37c2e939c0b83a08fd2c599d8e295
Mark Jabel Frias <friasmarkjabel@gmail.com>
2020-05-17 16:35:47 +0800

    Change requests to HTTPS

--- frontend/src/app/posts/posts.service.ts
+++ frontend/src/app/posts/posts.service.ts
@@ -26,27 +26,27 @@ export class PostsService {
   constructor(private http: HttpClient) { }
 
   getPosts() {
-    return this.http.get('http://heroku-speakout.herokuapp.com/posts/');
+    return this.http.get('https://heroku-speakout.herokuapp.com/posts/');
   }
   addPost(post: Post) {
-    return this.http.post('http://heroku-speakout.herokuapp.com/posts/', post);
+    return this.http.post('https://heroku-speakout.herokuapp.com/posts/', post);
  }
 
   // Returns results for trending posts
   getTrending() {
-    return this.http.get('http://heroku-speakout.herokuapp.com/posts/trending/');
+    return this.http.get('https://heroku-speakout.herokuapp.com/posts/trending/');
   }
 
   getSpecificPost(id: string) {
-    return this.http.get('http://heroku-speakout.herokuapp.com/posts/' + id);
+    return this.http.get('https://heroku-speakout.herokuapp.com/posts/' + id);
   }
 
   incrementLikes(id: string) {
-    return this.http.patch('http://heroku-speakout.herokuapp.com/posts/like/' + id, null);
+    return this.http.patch('https://heroku-speakout.herokuapp.com/posts/like/' + id, null);
   }
 
   getLikes(id: string) {
-    return this.http.get("http://heroku-speakout.herokuapp.com/posts/like-data/" + id);
+    return this.http.get("https://heroku-speakout.herokuapp.com/posts/like-data/" + id);
   }
   
 }
