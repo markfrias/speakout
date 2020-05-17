409afb757396cc812685855422c8f77dae0cc558
Mark Jabel Frias <friasmarkjabel@gmail.com>
2020-05-17 20:03:08 +0800

    Try to fix CORS

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,7 @@ app.use(passport.initialize());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*");
     res.header(
-        "Access-Control-Allow-Origin",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        "Access-Control-Allow-Origin"
     );
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
